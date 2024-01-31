e9ee8da6b3a38abfdacae38d6718aaa29aabd84f
alfathafp <alfathfebriadi@gmail.com>
2024-02-01 02:19:12 +0700

    add

--- src/main/java/org/example/TestCase1.java
+++ src/main/java/org/example/TestCase1.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class TestCase1 {
     @Test
     public void testMethod1(){
-        Assert.assertTrue(true);
+        Assert.assertTrue(false);
         System.out.println("test method1 is not working as expected");
     }
 }
