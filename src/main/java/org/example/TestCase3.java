2e693ebe3e7277b4a2e558d68708b5b7a0a749b0
alfathafp <alfathfebriadi@gmail.com>
2024-02-07 11:25:16 +0700

    add all needed

--- src/main/java/org/example/TestCase3.java
+++ src/main/java/org/example/TestCase3.java
@@ -7,6 +7,6 @@ public class TestCase3 {
     @Test
     public void testMethod3(){
         Assert.assertTrue(false);
-        System.out.println("test method2 is working as expected");
+        System.out.println("test method3 is working as expected");
     }
 }
