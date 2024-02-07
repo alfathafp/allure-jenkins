a265d2999b83a343a98c2edb0574dd7fa84805df
alfathafp <alfathfebriadi@gmail.com>
2024-02-07 11:18:54 +0700

    add test annotation

--- src/main/java/org/example/TestCase3.java
+++ src/main/java/org/example/TestCase3.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class TestCase3 {
+    @Test
     public void testMethod3(){
         Assert.assertTrue(false);
         System.out.println("test method2 is working as expected");
