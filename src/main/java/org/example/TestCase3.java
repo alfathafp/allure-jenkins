fa83dd6fd0948e83a2c4fcd552d2255cfd5ca140
alfathafp <alfathfebriadi@gmail.com>
2024-02-07 11:10:04 +0700

    add more xml

---
+++ src/main/java/org/example/TestCase3.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.testng.Assert;
+
+public class TestCase3 {
+    public void testMethod3(){
+        Assert.assertTrue(false);
+        System.out.println("test method2 is working as expected");
+    }
+}
