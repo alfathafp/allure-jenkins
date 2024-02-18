e80fa16b102b3c86c3472390f9e99e93b9a81a76
alfathafp <alfathfebriadi@gmail.com>
2024-02-18 22:27:02 +0700

    add lombok

--- src/main/java/org/example/TestCase1.java
+++ src/main/java/org/example/TestCase1.java
@@ -2,14 +2,18 @@ package org.example;
 import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@Slf4j
 public class TestCase1 {
     @Test
     public void testMethod1(){
         Assert.assertTrue(false);
-        System.out.println("test method1 is not working as expected");
+        log.info("test method1 is not working as expected");
+        String jenkinsURL = System.getenv("JENKINS_URL");
+        log.info("Jenkins URL: " + jenkinsURL);
     }
 
     @Getter
--- src/main/java/org/example/TestCase2.java
+++ src/main/java/org/example/TestCase2.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@Slf4j
 public class TestCase2 {
     @Test
     public void testMethod2(){
         Assert.assertTrue(true);
-        System.out.println("test method2 is working as expecter");
+        log.info("test method1 is not working as expected");
+        String jenkinsURL = System.getenv("JENKINS_URL");
+        log.info("Jenkins URL: " + jenkinsURL);
     }
 
 }
